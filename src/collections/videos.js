bfda92e4e47c76a8550746bb68c0ccda577e73c9
Hack Reactor Student <student@hackreactor.com>
2017-09-07 15:07:48 -0700

    Implements puppies

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,36 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  initialize: function(searchThing) {
+    var searchThing = searchThing || 'puppies';
+    //that perfoms an initial inquiry to the capital YOUTUBE peepz
+    this.search(searchThing);
+    console.log(this);
+  },
+
+  search: function (query) {
+    // SEARCH FUNCTION TO FETCH DATA FROM YOUTUBE'S API
+    this.fetch({
+      data: {
+        q: query,
+        part: 'snippet',
+        //maxResults: 5,
+        key: window.YOUTUBE_API_KEY, // in youtube.js
+        //videoEmbeddable: true,
+        //type: 'video', // needed to make embeddable only work
+      }
+    });
+    //we need to make this an array
+    //we need to select the first of these results
+  },
+
+  // url: 'https://www.googleapis.com/youtube/v3/videos'
+  url: 'https://www.googleapis.com/youtube/v3/search',
+
+  parse: function (data) {
+    return data.items;
+  }
+
 
 });
--- src/views/app.js
+++ src/views/app.js
@@ -4,13 +4,16 @@ var AppView = Backbone.View.extend({
 
   initialize: function(videoz) {
     this.videos = new Videos(videoz);
+    this.videos.search('puppies');
     this.render();
     this.VideoPlayerView = new VideoPlayerView({model: this.videos.first()}).render();
     this.VideoListView = new VideoListView({collection: this.videos}).render();
     this.SearchView = new SearchView().render();
     this.videos.on('select', (selectedVideo) => {
       this.VideoPlayerView = new VideoPlayerView({model: selectedVideo}).render();
-
+    });
+    this.videos.on('sync', () => {
+      this.VideoPlayerView = new VideoPlayerView({model: this.videos.first()}).render();
     });
   },
 
@@ -19,6 +22,8 @@ var AppView = Backbone.View.extend({
     return this;
   },
 
-  template: templateURL('src/templates/app.html')
+  template: templateURL('src/templates/app.html'),
+
+  // searchView:
 
 });
