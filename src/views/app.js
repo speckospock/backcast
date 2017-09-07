5377068ebf5ff06ada9204b1e6afefff29a976aa
Hack Reactor Student <student@hackreactor.com>
2017-09-07 16:15:15 -0700

    Implements search bar insta-search stuff'

--- src/views/app.js
+++ src/views/app.js
@@ -5,6 +5,7 @@ var AppView = Backbone.View.extend({
   initialize: function(videoz) {
     this.videos = new Videos(videoz);
     this.videos.search('puppies');
+    console.log(this.videos.first());
     //this.videos.search('baseball');
     this.render();
     this.VideoPlayerView = new VideoPlayerView({model: this.videos.first()}).render();
@@ -21,10 +22,21 @@ var AppView = Backbone.View.extend({
     //search button on click should call this.videos.search with the search query
     this.SearchView.$('.btn').on('click', () => {
       this.videos.search(this.SearchView.$('.form-control').val());
+
     });
     this.SearchView.$('.form-control').on('keypress', (e) => {
       if (e.which === 13) {
         this.SearchView.$('.btn').trigger('click');
+      } else {
+        //make a new videos object,
+        var videoSearch = new Videos();
+        videoSearch.search(this.SearchView.$('.form-control').val());
+        // (_.debounce(videoSearch.search(this.SearchView.$('.form-control').val())), 500);
+        //render a new videoListView with that new object?
+        this.VideoListView = new VideoListView({collection: videoSearch}).render();
+        videoSearch.on('select', (selectedVideo) => {
+          this.VideoPlayerView = new VideoPlayerView({model: selectedVideo}).render();
+        });
       }
     });
   },
